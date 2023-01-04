947c2472368e9cb55795383105836f67656ebf3a
Tyler Jensen <troubletimestwo97@gmail.com>
2023-01-04 16:04:16 -0700

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BiMenu } from "react-icons/bi";
+
+const Header = () => {
+  return (
+    <header>
+      <h2>Avanto</h2>
+      <BiMenu size="35px" />
+    </header>
+  );
+};
+
+export default Header;
